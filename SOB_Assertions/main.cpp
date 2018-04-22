f37f2cd525b94444a0e089b6a06b5686c5dd5d2b
WSudul <wojciech.sudul@gmail.com>
2018-04-22 15:41:54 +0200

    adding binary mode to arguments,  reading to list cleanup + error handling

--- SOB_Assertions/main.cpp
+++ SOB_Assertions/main.cpp
@@ -15,19 +15,23 @@ enum  TypeOption  {
 };
 
 namespace option {
-	constexpr auto arg_start = '-';
-	constexpr auto file = 'f';
-	constexpr auto type = 't';
-	constexpr auto range = 'r';
+	constexpr auto arg_start = '-'; //starts each argument below
+	constexpr auto file = 'f'; //path to file with data - needs to be followed by string
+	constexpr auto type = 't';  //type - tread data as inegers or doubles
+	constexpr auto range = 'r'; //range - needs to be followed by 2 numbers
+	constexpr auto binary_mode = 'b'; //optional - open as binary file
 
 	constexpr auto int_type = "int";
 	constexpr auto double_type = "double";
 
+
+
 	struct configuration {
-		std::string file_path="data.txt";
+		std::string file_path;
+		std::ios_base::openmode mode=std::ios::in;
 		TypeOption type_option = TypeOption::UNKNOWN;
-		std::string min="-100";
-		std::string max="100";
+		std::string min;
+		std::string max;
 	};
 }
 
@@ -36,58 +40,89 @@ template<typename T>
 void TestLinkedList(std::unique_ptr<SortedLinkedList<T>>& list);
 void TestFileWrapper();
 option::configuration ParseArguments(int argc, char** argv);
+bool VerifyConfiguration(option::configuration);
 
 /*
 
 example of arguments via console:
--f test.txt -r 10 300 -t int
+-f ../SOB_Assertions/resources/numbers.txt -r 0 5 -t double
 */
 
 int main(int argc, char** argv) {
-
-	std::unique_ptr<IFileWrapper> file_wrapper = std::make_unique<FileWrapper>();
-	std::unique_ptr<SortedLinkedList<double>> linked_list_double = std::make_unique<SortedLinkedList<double>>();
-	std::unique_ptr<SortedLinkedList<int>> linked_list_int = std::make_unique<SortedLinkedList<int>>();
-	std::unique_ptr<SortedLinkedList<int>> linked_list_int_customer_comparator = std::make_unique<SortedLinkedList<int>>([](const int& v1, const int&v2) {return v1<v2; });
 	std::cout << "Hello" << std::endl;
+
+
+
 
 
 	option::configuration config;
 	config = ParseArguments(argc, argv);
-	/* test config*/
-	config = { "data.txt",TypeOption::INT,"-100","100" };
 
-	
-	assert(!config.file_path.empty());
-	assert(!config.min.empty());
-	assert(!config.max.empty());
-	assert((std::stod(config.max) - std::stod(config.min)) >= 0);
-	assert(TypeOption::UNKNOWN != config.type_option);
-	std::cout << config.file_path << "\t" << config.type_option << "\t" << config.min << "\t" << config.max << std::endl;
+	/*TEST DATA*/
+	//config = { "data.txt",(std::ios::in | std::ios::binary),TypeOption::INT,"-100","100" };
+
+	std::cout << config.file_path << "\t" << config.mode << "\t"
+                  << config.type_option << "\t" << config.min << "\t"
+                  << config.max << std::endl;
+
+	std::unique_ptr<IFileWrapper> file_wrapper = std::make_unique<FileWrapper>();
+	assert(file_wrapper->Open(config.file_path, config.mode));
 
 	std::vector<std::string> words;
+	while (!file_wrapper->EndOfFile()) {
+		std::string word = file_wrapper->ReadWord();
+		assert(file_wrapper->IsOk());	
+
+		if (!word.empty())
+			words.push_back(word);
+	}
 
 	switch(config.type_option) {
 	case TypeOption::INT: 
 	{
-		for (auto& word : words) {
-			int value = std::stoi(word);
-			linked_list_int->Add(value);
+		std::unique_ptr<SortedLinkedList<int>> linked_list_int = std::make_unique<SortedLinkedList<int>>();
+		try {
+			int min = std::stoi(config.min);
+			int max = std::stoi(config.max);
+			std::cout << "Reading to integer linked list" << std::endl;
+			for (auto& word : words) {
+				int value = std::stoi(word);
+				if (value >= min && value <= max)
+					linked_list_int->Add(value);
+			}
 		}
-		std::cout << linked_list_int->ToString();
-		std::cout << linked_list_int->ReversedToString();
+		catch (std::invalid_argument& e) {
+
+			std::cout << "Exception caught while parsing data to selected type: " << e.what() << std::endl;
+			return 0;
+
+		}
+		std::cout << linked_list_int->ToString() << std::endl;;
+		std::cout << linked_list_int->ReversedToString() << std::endl;
 
 
 	}
 	break;
 	case TypeOption::DOUBLE:
-	{
-		for (auto& word : words) {
-			double value = std::stod(word);
-			linked_list_double->Add(value);
+	{		
+		std::unique_ptr<SortedLinkedList<double>> linked_list_double = std::make_unique<SortedLinkedList<double>>();
+		try {
+			double min = std::stod(config.min);
+			double max = std::stod(config.max);
+			std::cout << "Reading to double linked list" << std::endl;
+			for (auto& word : words) {
+				double value = std::stod(word);
+				if (value >= min && value <= max)
+					linked_list_double->Add(value);
+			}
+		}catch (std::invalid_argument& e) {
+
+			std::cout << "Exception caught while parsing data to selected type: " << e.what() << std::endl;
+			return 0;
+
 		}
-		std::cout << linked_list_double->ToString();
-		std::cout << linked_list_double->ReversedToString();
+		std::cout << linked_list_double->ToString() << std::endl;;
+		std::cout << linked_list_double->ReversedToString() << std::endl;
 
 	}
 	break;
@@ -95,22 +130,15 @@ int main(int argc, char** argv) {
 		std::cout << "Unsupported option activated" << std::endl;
 	}
 
-	//cleanup for tests
-	linked_list_int.reset(new SortedLinkedList<int>());
-	linked_list_double.reset(new SortedLinkedList<double>());
-
+	//TESTING
+	std::unique_ptr<SortedLinkedList<int>> linked_list_int_customer_comparator = std::make_unique<SortedLinkedList<int>>([](const int& v1, const int&v2) {return v1 < v2; });
+	std::unique_ptr<SortedLinkedList<int>> linked_list_int = std::make_unique<SortedLinkedList<int>>();
+	std::unique_ptr<SortedLinkedList<double>> linked_list_double = std::make_unique<SortedLinkedList<double>>();
 	TestLinkedList(linked_list_double);
 	TestLinkedList(linked_list_int);
 	TestLinkedList(linked_list_int_customer_comparator);
-	//TestFileWrapper();
+	TestFileWrapper();
 
-	std::cout << file_wrapper->Open("numbers.txt", std::ios::in) << std::endl;
-	std::vector<std::string> words1 = file_wrapper->ReadWordsToVector();
-
-	for (int i = 0; i<words1.size(); ++i)
-		std::cout << words1[i] << ' ';
-	file_wrapper->Close();
-	getchar();
 	return 0;
 }
 
@@ -152,12 +180,13 @@ void TestLinkedList(std::unique_ptr<SortedLinkedList<T>>& list) {
 
 void TestFileWrapper() {
 	std::cout << "*---Start of test---*" << std::endl;
+	std::cout << "*---File wrapper----*" << std::endl;
 	std::unique_ptr<IFileWrapper> file_wrapper = std::make_unique<FileWrapper>();
 	assert(!file_wrapper->Open("dummy_non_existing_file.dummy",std::ios::in));
 	assert(!file_wrapper->Close());
 
 	//open binary file and read some value
-	assert(file_wrapper->Open("binary_file_test", std::ios::in | std::ios::binary));
+	assert(file_wrapper->Open("../SOB_Assertions/resources/binary_file_test", std::ios::in | std::ios::binary));
 	assert(file_wrapper->IsOk());
 	assert(!file_wrapper->EndOfFile());
 
@@ -174,6 +203,16 @@ void TestFileWrapper() {
 	assert(file_wrapper->EndOfFile());
 	assert(file_wrapper->Close());
 
+
+
+	std::cout << file_wrapper->Open("../SOB_Assertions/resources/numbers.txt", std::ios::in) << std::endl;
+	std::vector<std::string> words1 = file_wrapper->ReadWordsToVector();
+
+	for (int i = 0; i<words1.size(); ++i)
+		std::cout << words1[i] << ' ';
+	file_wrapper->Close();
+
+
 	std::cout << "*-----End of test---*" << std::endl;
 	std::cout << "*-------------------*" << std::endl;
 
@@ -181,7 +220,7 @@ void TestFileWrapper() {
 
 option::configuration ParseArguments(int argc, char** argv) {
 
-	//assert(argc > 2);
+	assert(argc > 2);
 	option::configuration config;
 
 
@@ -212,9 +251,15 @@ option::configuration ParseArguments(int argc, char** argv) {
 				config.min = argv[i + 1];
 				config.max = argv[i + 2];
 
-				i += 2;
 				assert(!config.min.empty());
 				assert(!config.max.empty());
+
+				i += 2;
+
+			}
+			else if (argument.at(1) == option::binary_mode) {
+				
+				config.mode |= std::ios::binary;
 			}
 			else
 			{
@@ -229,3 +274,21 @@ option::configuration ParseArguments(int argc, char** argv) {
 
 }
 
+bool VerifyConfiguration(const option::configuration& config){
+
+	//verify configuration
+	assert(!config.file_path.empty());
+	assert(0 != config.mode);
+	assert(!config.min.empty());
+	assert(!config.max.empty());
+	assert(TypeOption::UNKNOWN != config.type_option);
+	try {
+		assert((std::stod(config.max) - std::stod(config.min)) >= 0);
+	}
+	catch (std::invalid_argument& ex) {
+		std::cout << "Exception caught: " << ex.what() << std::endl;
+		return false;
+	}
+	return true;
+
+}
